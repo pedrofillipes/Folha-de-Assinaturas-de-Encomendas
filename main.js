7c463958ccd57ffa7d5d9b1151454b207f2dddcc
Pedro Oliveira <117683583+pedrofillipes@users.noreply.github.com>
2025-06-23 11:53:14 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -60,12 +60,12 @@ function gerarTabela() {
           word-wrap: break-word;
         }
         th:nth-child(1), td:nth-child(1) { width: 10%; }
-        th:nth-child(2), td:nth-child(2) { width: 15%; }
-        th:nth-child(3), td:nth-child(3) { width: 10%; font-weight: bold; }
-        th:nth-child(4), td:nth-child(4) { width: 20%; }
-        th:nth-child(5), td:nth-child(5) { width: 20%; font-weight: bold; }
+        th:nth-child(2), td:nth-child(2) { width: 12%; }
+        th:nth-child(3), td:nth-child(3) { width: 7%; font-weight: bold; }
+        th:nth-child(4), td:nth-child(4) { width: 15%; }
+        th:nth-child(5), td:nth-child(5) { width: 25%; font-weight: bold; }
         th:nth-child(6), td:nth-child(6) {
-          width: 25%;
+          width: 30%;
           text-align: left;
         }
         @media print {
